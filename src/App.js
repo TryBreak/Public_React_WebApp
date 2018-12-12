c651d49dcef8df1710149301bc0ad1f6f53b3e86
Mark-Pro <meichangliang@outlook.com>
2018-12-12 18:35:22 +0800

    'add'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  addTodo,
+  completeTodo,
+  setVisibilityFilter,
+  VisibilityFilters
+} from "./actions";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import Footer from "./components/Footer";
 
 class App extends Component {
+  logFun = text => {
+    console.log("add todo", text);
+  };
   render() {
     return (
       <div>
-        <AddTodo onAddClick={text => console.log("add todo", text)} />
+        <AddTodo onAddClick={text => this.logFun(text)} />
         <TodoList
           todos={[
             {
@@ -29,4 +39,22 @@ class App extends Component {
     );
   }
 }
-export default App;
+function selectTodos(todos, filter) {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos;
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(todo => todo.completed);
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(todo => !todo.completed);
+    default:
+      return filter;
+  }
+}
+function select(state) {
+  return {
+    visibleTodos: selectTodos(state.todos, state.visibilityFilter),
+    visibilityFilter: state.visibilityFilter
+  };
+}
+export default connect(select)(App);

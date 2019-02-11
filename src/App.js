6e83757855130a1d97e340a7511059166a9589c1
Mark <meichangliang@outlook.com>
2019-02-11 19:54:54 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -1,73 +1,28 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import {
-  addTodo,
-  completeTodo,
-  setVisibilityFilter,
-  VisibilityFilters
-} from "./actions";
-import AddTodo from "./components/AddTodo";
-import TodoList from "./components/TodoList";
-import Footer from "./components/Footer";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    // Injected by connect() call:
-    const { dispatch, visibleTodos, visibilityFilter } = this.props;
     return (
-      <div>
-        <AddTodo onAddClick={text => dispatch(addTodo(text))} />
-        <TodoList
-          todos={visibleTodos}
-          onTodoClick={index => dispatch(completeTodo(index))}
-        />
-        <Footer
-          filter={visibilityFilter}
-          onFilterChange={nextFilter =>
-            dispatch(setVisibilityFilter(nextFilter))
-          }
-        />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
 }
 
-App.propTypes = {
-  visibleTodos: PropTypes.arrayOf(
-    PropTypes.shape({
-      text: PropTypes.string.isRequired,
-      completed: PropTypes.bool.isRequired
-    }).isRequired
-  ).isRequired,
-  visibilityFilter: PropTypes.oneOf([
-    "SHOW_ALL",
-    "SHOW_COMPLETED",
-    "SHOW_ACTIVE"
-  ]).isRequired
-};
-
-function selectTodos(todos, filter) {
-  switch (filter) {
-    case VisibilityFilters.SHOW_ALL:
-      return todos;
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter(todo => todo.completed);
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter(todo => !todo.completed);
-    default:
-      return filter;
-  }
-}
-
-// Which props do we want to inject, given the global state?
-// Note: use https://github.com/faassen/reselect for better performance.
-function select(state) {
-  return {
-    visibleTodos: selectTodos(state.todos, state.visibilityFilter),
-    visibilityFilter: state.visibilityFilter
-  };
-}
-
-// 包装 component ，注入 dispatch 和 state 到其默认的 connect(select)(App) 中；
-export default connect(select)(App);
+export default App;

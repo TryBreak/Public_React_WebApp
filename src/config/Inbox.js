7bbd622e870ee388cbb0495a08a1f255b9dfb377
Mark <meichangliang@outlook.com>
2019-03-20 16:14:37 +0800

    ~

--- src/config/Inbox.js
+++ src/config/Inbox.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import routes from "../views/routes";
+
+const route_list_view = () => {
+  return routes.map((item, index) => {
+    return (
+      <div key={item.name}>
+        <Link to={item.path}>{item.name}</Link>
+      </div>
+    );
+  });
+};
 class Inbox extends Component {
   render() {
     return (
       <div>
-        <Link to="/about">About</Link>
-        <br />
-        <Link to="/home">Home</Link>
-        <br />
-        <button onClick={() => this.props.history.push("about")}>
+        {route_list_view()}
+        {/* <button onClick={() => this.props.history.push("about")}>
           通过函数跳转
-        </button>
-        <br />
+        </button> */}
       </div>
     );
   }

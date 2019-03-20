cc8aebd9ac02812ee2f02f3829bf3e4982f79c30
Mark <meichangliang@outlook.com>
2019-03-20 14:16:43 +0800

    router

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "react-app-polyfill/ie9";
+import Router from "./Router";
+import "./index.css";
 
-import { BrowserRouter, Route, Link } from "react-router-dom";
-
-console.log(BrowserRouter);
-console.log(Route);
-console.log(Link);
-
-// import About from "./demo/About";
-// import Inbox from "./demo/Inbox";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Router />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

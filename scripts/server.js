5b1ddcb875826a5aec5693755d9a7894686f6df0
Mark <meichangliang@outlook.com>
2019-04-04 15:15:58 +0800

    static~

--- scripts/server.js
+++ scripts/server.js
@@ -1,10 +1,13 @@
+const static = require('koa-static');
 const Koa = require('koa');
 const app = new Koa();
-const path = require('path');
 
-//静态资源服务器
-const static = require('./middleware/static');
-app.use(static);
+const staticPath = '../build';
 
+app.use(static(path.join(__dirname, staticPath)));
 app.listen(3000);
-console.log('[demo] start-quick is starting at port 3000');
+
+app.listen(3380, '0.0.0.0', () => {
+  console.log('server is starting at port 2333');
+  console.log('服务已经启动,请在浏览器中输入 http://localhost:2333 ');
+});

4eb48e4e59dbb40b1364080de27d34f1929a4ab3
Mark <meichangliang@outlook.com>
2019-04-04 16:06:33 +0800

    ~

--- scripts/server.js
+++ scripts/server.js
@@ -1,13 +1,23 @@
 const static = require('koa-static');
 const Koa = require('koa');
 const app = new Koa();
+const path = require('path');
+const fs = require('fs');
 
 const staticPath = '../build';
 
 app.use(static(path.join(__dirname, staticPath)));
-app.listen(3000);
 
-app.listen(3380, '0.0.0.0', () => {
-  console.log('server is starting at port 2333');
-  console.log('服务已经启动,请在浏览器中输入 http://localhost:2333 ');
+app.use(async ctx => {
+  const html = fs.readFileSync(
+    path.join(__dirname, staticPath, 'index.html'),
+    'utf-8'
+  );
+  ctx.body = html;
+});
+
+const port = 3380;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`server is starting at port ${port}`);
+  console.log(`服务已经启动,请在浏览器中输入 http://localhost:${port} `);
 });

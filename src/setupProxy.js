7c7195789db16af4efd4bbfc53526a4b0fe104a4
Mark <meichangliang@outlook.com>
2019-04-14 12:34:28 +0800

    ~!

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,9 @@
 const proxy = require('http-proxy-middleware');
 
+const proxyConfig = proxy('/rank', {
+  target: 'http://192.168.2.29:50051',
+});
+
 module.exports = function(app) {
-  app.use(
-    proxy('/rank', {
-      target: 'http://192.168.2.29:50051',
-    })
-  );
+  app.use(proxyConfig);
 };

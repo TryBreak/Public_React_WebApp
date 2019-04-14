925114d6b4c94057a268f365819e76a7cbfdf9c8
Mark <meichangliang@outlook.com>
2019-04-14 14:43:08 +0800

    ~

--- src/views/Demo/StyleDemo/CssStyleDemo/index.js
+++ src/views/Demo/StyleDemo/CssStyleDemo/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import styles from './index.module.less';
-import image14 from '../../../../assets/image/14.jpg';
+import image14 from '@assets/image/14.jpg';
 class CssStyleDemo extends Component {
   render() {
     return (
       <div>
         <h1>这里是 CssStyleDemo ,这里是三级路由</h1>
-        img标签引用
-        <img src={require('../../../../assets/image/13.jpg')} alt="" />
-        <img src={image14} alt="" />
+        img标签引用:
+        <img src={require('@assets/image/13.jpg')} alt="" />
+        <img src={image14} alt="image14" />
         css使用
         <div className={styles.cover} />
         less 函数单位计算
--- src/views/routes.js
+++ src/views/routes.js
@@ -1,11 +1,4 @@
-const _import = url => {
-  let Template = require('.' + url);
-  if (Template && Template.default) {
-    return Template.default;
-  } else {
-    console.error('页面路由组件引用出现了问题!! -- ' + url);
-  }
-};
+import _import from '@src/utils/_import';
 
 const routes = [
   {

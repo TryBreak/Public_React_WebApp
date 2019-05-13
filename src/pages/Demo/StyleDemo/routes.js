bb696625347056c6f8e631b48c3c076dab800743
Mark <meichangliang@outlook.com>
2019-05-13 18:26:57 +0800

    渲染层级完成~

--- src/pages/Demo/StyleDemo/routes.js
+++ src/pages/Demo/StyleDemo/routes.js
@@ -3,17 +3,12 @@
  * @Description: In User Settings Edit
  * @Author: Mark
  * @Date: 2019-04-08 11:33:38
- * @LastEditTime: 2019-05-13 18:24:11
+ * @LastEditTime: 2019-05-13 18:26:36
  */
-const _import = url => {
-  let Template = require('.' + url);
-  if (Template && Template['default']) {
-    return Template['default'];
-  } else {
-    console.error('页面路由组件引用出现了问题!! -- ' + url);
-  }
-};
-// import _import from '@/utils/_import';
+import _import from '@/utils/_import';
+
+const basePath = '/Demo/StyleDemo';
+const fatherPath = '/demo/style_demo';
 
 const routes = [
   {
@@ -21,14 +16,16 @@ const routes = [
     name: 'css样式演示',
     description: 'css样式演示',
     title: '三级路由-样式演示',
-    component: _import('/CssStyleDemo'),
+    component: _import(basePath + '/CssStyleDemo'),
+    from: fatherPath,
   },
   {
     path: '/js_style_demo',
     name: 'js样式演示',
     description: 'js样式演示',
     title: '',
-    component: _import('/JsStyleDemo'),
+    component: _import(basePath + '/JsStyleDemo'),
+    from: fatherPath,
   },
 ];
 

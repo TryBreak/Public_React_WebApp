0972980b3cd5a26c1695e6c56fcc5062ff9f8c54
Mark <meichangliang@outlook.com>
2019-05-17 15:03:00 +0800

    update

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,6 +3,7 @@
  * @Description: 全局的参数
  * @Author: Mark
  * @Date: 2019-05-05 11:53:31
- * @LastEditTime: 2019-05-17 14:51:03
+ * @LastEditTime: 2019-05-17 15:01:47
  */
 export const project_detail = { name: process.env.REACT_APP_PROJECT_NAME };
+export const html_fs = process.env.REACT_APP_HTML_FONTSIZE;
